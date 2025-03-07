dd77025bc440754001f0e7428528eda3c4aaf6bb
Seob <leeminseob@gmail.com>
2025-03-07 22:27:37 +0900

    코드 전체 리팩토링

--- Bash/Change_CentOS_password_algorithm.sh
+++ Bash/Change_CentOS_password_algorithm.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-# CentOS 6 이하에서 알고리즘을 변경하기 위해 사용
-RESULT=`authconfig --test | grep algorithm | awk '{print $5}'`
+# CentOS 6 이하에서 알고리즘을 변경하는 스크립트
 
-if [ RESULT == 'md5' ]; then
+RESULT=$(authconfig --test | grep algorithm | awk '{print $5}')
+
+if [ "$RESULT" = "md5" ]; then
     authconfig --passalgo=sha512 --update
-    AFTER_RESULT=`authconfig --test | grep algorithm | awk '{print $5}'`
-    echo `hostname` : "Done -" $RESULT "-->" $AFTER_RESULT
-    exit 0
-elif [ RESULT == 'sha512' ]; then
-    echo `hostname` : "You don not need change to algorithm on this server -" $RESULT
-    exit 0
+    AFTER_RESULT=$(authconfig --test | grep algorithm | awk '{print $5}')
+    echo "$(hostname): Password algorithm updated - $RESULT → $AFTER_RESULT"
+elif [ "$RESULT" = "sha512" ]; then
+    echo "$(hostname): No change needed - Current algorithm is $RESULT"
 else
-    echo `hostname` : "Failed"
-    exit 0
+    echo "$(hostname): Failed to detect password algorithm"
+    exit 1
 fi
